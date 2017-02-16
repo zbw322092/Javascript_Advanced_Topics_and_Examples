b2c90a66daed75516502290dc9d8dbd0990ee818
zbw322092 <bz4e14@soton.ac.uk>
2017-02-16 11:51:06 +0800

    example without using call or apply

--- bind_call_apply.js
+++ bind_call_apply.js
@@ -85,6 +85,60 @@ greetAYoungster('Alex');
 
 
 
+/* example five */
+var avgScore = "global avgScore";
+
+function avg(arrayOfScores) {
+	var sumOfScores = arrayOfScores.reduce(function(prev, cur, index, array) {
+		return prev + cur;
+	});
+
+	this.avgScore = sumOfScores / arrayOfScores.length; 
+}
+
+var gameController = {
+	scores:[20, 34, 55, 46, 77],
+	avgScore: null
+};
+
+avg(gameController.scores);
+
+console.log(window.avgScore); // 46.4
+console.log(gameController.avgScore); // null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
