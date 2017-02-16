ebd8b450adf8cd1801a1d8152a498df806387a3d
zbw322092 <bz4e14@soton.ac.uk>
2017-02-16 12:01:15 +0800

    using apply to set this explicitly

--- bind_call_apply.js
+++ bind_call_apply.js
@@ -108,6 +108,22 @@ console.log(gameController.avgScore); // null
 
 
 
+/* example six */
+var clientData = {
+	id: 094545,
+  fullName: "Not Set",
+  setUserName: function(firstName, lastName) {
+  	this.fullName = firstName + " " + lastName; 
+  }
+};
+
+function getUserInput(firstName, lastName, callback, callbackObj) {
+	callback.apply(callbackObj, [firstName, lastName]);
+}
+
+getUserInput('Bowen', 'Zhong', clientData.setUserName, clientData);
+console.log(clientData);
+
 
 
 
